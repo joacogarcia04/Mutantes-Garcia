85fde894dbeeb280434ddbcb8db030312a5206ae
joacogarcia04 <joacogarcia1908@gmail.com>
2025-11-24 10:20:36 -0300

    primer commit

---
+++ src/main/java/org/example/mutantesJG/MutantesJgApplication.java
@@ -0,0 +1,13 @@
+package org.example.mutantesJG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutantesJgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutantesJgApplication.class, args);
+	}
+
+}
